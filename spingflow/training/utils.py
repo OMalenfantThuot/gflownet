e745ae6de1c738425f8e16d897bf0b93db66c9d1
Olivier Malenfant-Thuot <malenfantthuotolivier@gmail.com>
2023-05-24 16:56:56 -0400

    added tensorboard logging

--- spingflow/training/utils.py
+++ spingflow/training/utils.py
@@ -1,5 +1,7 @@
-import torch
 import argparse
+import os
+import torch
+from torch.utils.tensorboard import SummaryWriter
 from spingflow.modeling.utils import add_modeling_arguments_to_parser
 
 
@@ -40,4 +42,28 @@ def create_train_parser():
     parser.add_argument("--patience", type=int, help="Scheduler patience")
     parser.add_argument("--factor", type=float, help="Scheduler reduction factor")
     parser.add_argument("--device", choices=["cpu", "cuda"], help="Training device")
+    parser.add_argument(
+        "--log_dir",
+        default="../../logs",
+        help="Top level directory for Tensorboard logs",
+    )
+    parser.add_argument("--run_name", default=None, help="Name of the run")
     return parser
+
+
+def create_summary_writer(args):
+    if args.run_name:
+        base_log_dir = os.path.join(args.log_dir, args.run_name)
+    else:
+        base_log_dir = os.path.join(args.log_dir, f"N{args.N}_T{args.temperature}")
+
+    log_dir_counter = 0
+    while True:
+        test_log_dir = base_log_dir + f"_{log_dir_counter:04}"
+        if os.path.exists(test_log_dir):
+            log_dir_counter += 1
+        else:
+            break
+
+    writer = SummaryWriter(log_dir=test_log_dir)
+    return writer
